cb3bc3e04383ff58e6583373d643e95925351125
Steven Barry SE2015 <steven.barry.2016@uni.strath.ac.uk>
2018-04-15 23:42:20 +0100

    used the JS encodeURIComponent function to get the address entered by the user in the terminal when the program is run. Then the program will output the full address entered by the user.

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const request = require ("request");
+const yargs = require("yargs");
+
+const argv = yargs
+  .options({
+      a: {
+        demand: true,
+        alias: "address",
+        describe: "address to fetch weather for",
+        string: true
+        //string: true = always parse argument as string
+      }
+  })
+  .help()
+    //set alias for help command as "h"
+  .alias("help", "h")
+  .argv;
+
+//var gets user address entered by the user frkm the terminal when the prog is argument
+//such as-  node app.js -a "flat 11 bute court 30 dirleton drive glasgow"
+var encodedAddress = encodeURIComponent(argv.address);
+
+console.log(argv);
 
 request({
-  url: "https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia",
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   //convert string data into json object
   json: true
 }, (error, response, body) => {
