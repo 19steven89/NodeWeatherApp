011675d8bc31789f8e3f57002fc913d6befd307a
Steven Barry SE2015 <steven.barry.2016@uni.strath.ac.uk>
2018-05-14 15:51:24 +0100

    used request library to access the dark skies weather api from the forecast.io website. using this have output the current temperature based on my latitude and longitude.

--- app.js
+++ app.js
@@ -1,28 +1,50 @@
-const yargs = require("yargs");
-const geocode = require("./GeoCode/geocode.js")
+// const yargs = require("yargs");
+// const geocode = require("./GeoCode/geocode.js")
+//
+// const argv = yargs
+//   .options({
+//       a: {
+//         demand: true,
+//         alias: "address",
+//         describe: "address to fetch weather for",
+//         string: true
+//         //string: true = always parse argument as string
+//       }
+//   })
+//   .help()
+//     //set alias for help command as "h"
+//   .alias("help", "h")
+//   .argv;
 
-const argv = yargs
-  .options({
-      a: {
-        demand: true,
-        alias: "address",
-        describe: "address to fetch weather for",
-        string: true
-        //string: true = always parse argument as string
+// //pass in the address entered in the CLI to the function
+// //then have the callback function (errorMsg, results) => to
+// //deal with error msg or the result which will be the Lat and Long values
+// geocode.geocodeAddress(argv.address, (errorMsg, results) => {
+//   if(errorMsg){
+//     console.log(errorMsg);
+//   }else if(results){
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
+
+
+//api key 7505323d970265e8ebe0f6268fd43cb4
+//https://api.forecast.io/forecast/7505323d970265e8ebe0f6268fd43cb4/55.84493320000001,-4.2928336
+
+const request = require("request");
+
+request(
+  "https://api.forecast.io/forecast/7505323d970265e8ebe0f6268fd43cb4/55.84493320000001,-4.2928336",
+  {json: true},
+    //the callback function gets fired when the url request gets made
+    (err, response, body) => {
+      if (err) {
+        return console.log("Unable to connect to forecast.io servers");
+      }else if(response.statusCode === 400){
+          console.log("unable to fetch weather");
+      }else if(response.statusCode === 200){
+        var cel = (body.currently.temperature - 32) * 5 / 9
+        console.log("Current temperature is: " + body.currently.temperature + " Celsius: " + parseFloat(cel).toFixed(2));
+        console.log("Hourly Summary: " + body.minutely.summary);
       }
-  })
-  .help()
-    //set alias for help command as "h"
-  .alias("help", "h")
-  .argv;
-
-//pass in the address entered in the CLI to the function
-//then have the callback function (errorMsg, results) => to
-//deal with error msg or the result which will be the Lat and Long values
-geocode.geocodeAddress(argv.address, (errorMsg, results) => {
-  if(errorMsg){
-    console.log(errorMsg);
-  }else if(results){
-    console.log(JSON.stringify(results, undefined, 2));
-  }
-});
+    });
